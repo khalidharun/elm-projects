2a5b2ae6a9177b4ffc1f5a17d67f6eb9d76ce315
Khalid Harun <khalid.harun@gmail.com>
2018-03-28 00:03:17 -0500

    Make lots of updates

--- minesweeper.elm
+++ minesweeper.elm
@@ -1,38 +1,35 @@
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, text, h1)
 import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onWithOptions)
+import Json.Decode as Json
 import Dict
 import Array
 import Debug
 
-type alias Model =
-  { game : Game
-  , board: Board
-  }
-
-type alias Game =
-  { height: Int
-  , width: Int
-  , squares: Array.Array Bool
-  }
-
-type alias Board =
-  { height: Int
-  , width: Int
-  , squares: Array.Array Square
-  }
-
-type alias Square =
-  { visibility: Visibility
-  , score: Maybe Int
-  }
-
-type Visibility = Hidden | Showing
-
-type alias Msg = (Int, Int)
+main =
+  Html.beginnerProgram { model = model , view = view , update = update }
 
 model : Model
-model =
+model = resetGame
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    ResetGame -> resetGame
+    Reveal point -> { model | board = revealSquare model.board point }
+    Flag point -> { model | board = flagSquare model.board point }
+
+type alias Model = { game : Game , board: Board }
+type alias Game  = { height: Int , width: Int , squares: Array.Array Bool }
+type alias Board = { height: Int , width: Int , squares: Array.Array Square }
+type alias Square = { visibility: Visibility , score: Maybe Int }
+type alias Point = (Int, Int)
+type Visibility = Hidden | Uncovered | Flagged
+
+type Msg = ResetGame | Reveal Point | Flag Point
+
+resetGame : Model
+resetGame =
   let
     game = emptyGame 5 5
   in
@@ -52,21 +49,25 @@ emptyGame height width =
   in
       Game height width squares
 
-getIndex : { a | width: Int } -> (Int, Int) -> Int
-getIndex board (row, col) = (row * board.width) + col
+getIndex : { a | width: Int } -> Point -> Int
+getIndex board (row, col) =
+  (row * board.width) + col
 
-add (a,b) (c,d) = (a + c, b + d)
+add : Point -> Point -> Point
+add (a,b) (c,d) =
+  (a + c, b + d)
 
-isOutOfBounds : { a | height: Int, width: Int } -> (Int, Int) -> Bool
+isOutOfBounds : { a | height: Int, width: Int } -> Point -> Bool
 isOutOfBounds game (i, j) =
   if i >= 0 && i < game.height && j >= 0 && j < game.width then
     True
   else
     False
 
-computeSquare : Game -> (Int, Int) -> Square
-computeSquare game (row, col) =
+computeSquare : Game -> Point -> Square
+computeSquare game point =
   let
+      (row, col) = point
       offset =
         [ (-1, -1), (-1, 0), (-1, 1)
         , ( 0, -1),          ( 0, 1)
@@ -79,11 +80,9 @@ computeSquare game (row, col) =
       label = "Index (" ++ toString(row) ++ ", " ++ toString(col) ++ ")"
       total =
         offset
-        |> List.map (add (row, col))
+        |> List.map (add point)
         |> List.filter (isOutOfBounds game)
-        |> Debug.log label
         |> List.map (getIndex game)
-        |> Debug.log label
         |> List.map (flip Array.get game.squares)
         |> List.map toInt
         |> List.sum
@@ -105,24 +104,82 @@ computeBoard game =
   in
       Board game.height game.width squares
 
-update : Msg -> Model -> Model
-update msg model =
-  model
-
 getScore : Maybe Square -> String
 getScore maybeSquare =
   case maybeSquare of
     Just square ->
-      case square.score of
-        Nothing -> "x"
-        Just n -> toString(n)
+      case square.visibility of
+        Hidden -> " "
+        Flagged -> "F"
+        Uncovered ->
+          case square.score of
+            Nothing -> "E"
+            Just n -> toString(n)
     _ -> "Error"
+
+
+isFlagged : Square -> Bool
+isFlagged square =
+  case square.visibility of
+    Flagged -> True
+    _ -> False
+
+computeRemaining : Model -> Int
+computeRemaining model =
+  let
+      numTotal =
+        model.game.squares
+        |> Array.filter (\n -> True == n)
+        |> Array.length
+
+      numFlagged =
+        model.board.squares
+        |> Array.filter isFlagged
+        |> Array.length
+  in
+    numTotal - numFlagged
+
+onRightClick message =
+    onWithOptions
+        "contextmenu"
+            { stopPropagation = True
+               , preventDefault = True
+                }
+                    (Json.succeed message)
+
+revealSquare : Board -> Point -> Board
+revealSquare =
+  updateSquare Uncovered
+
+flagSquare : Board -> Point -> Board
+flagSquare =
+  updateSquare Flagged
+
+updateSquare : Visibility -> Board -> Point -> Board
+updateSquare visibility board point =
+  let
+      (row, col) = point
+      idx = getIndex board point
+      curr =
+        case Array.get idx board.squares of
+          Just x -> x
+          Nothing -> Square Hidden Nothing
+      next = { curr | visibility = visibility }
+      squares = Array.set idx next board.squares
+  in
+      { board | squares = squares }
 
 drawSquare : Board -> Int -> Int -> Html.Html Msg
 drawSquare board row col =
   let
-    index = getIndex board (row, col)
-    renderButton x = button [onClick (row, col)] [text x]
+      point = (row, col)
+      index = getIndex board point
+      renderButton x =
+        button  [ class "square"
+                , onClick (Reveal point)
+                , onRightClick (Flag point)
+                ]
+                [ text x ]
   in
       board.squares
       |> Array.get index
@@ -145,18 +202,24 @@ drawBoard board =
 
 drawButtons : Html.Html Msg
 drawButtons =
-  div [] [ button [] [ text "New Game" ] ]
+  div [ class "buttons" ] [ button [ onClick ResetGame ] [ text "New Game" ] ]
 
+drawCounter : Model -> Html.Html Msg
+drawCounter model =
+  let
+      msg =
+        model
+        |> computeRemaining
+        |> toString
+        |> ((++) "Mines Remaining: ")
+  in
+    div [ class "counter" ] [ text msg ]
+
+view : Model -> Html.Html Msg
 view model =
   div []
-  [ text "Minesweeper"
-  , div [] [drawBoard model.board]
-  , div [] [drawButtons]
+  [  h1 [] [ text "Minesweeper" ]
+  , drawCounter model
+  , drawBoard model.board
+  , drawButtons
   ]
-
-main =
-  Html.beginnerProgram
-  { model = model
-  , view = view
-  , update = update
-  }
